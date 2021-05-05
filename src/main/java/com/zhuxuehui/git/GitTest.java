c809747a88585bb84016e7503b6f7fbea1e79056
toto <941832449@qq.com>
2021-05-05 18:42:45 +0800

    first commit

---
+++ src/main/java/com/zhuxuehui/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.zhuxuehui.git;
+
+/**
+ * @author toto
+ * @version 1.0
+ * @date 2021-05-05 17:52
+ * @description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.err.println("hello git");
+    }
+}
---
+++ src/main/java/com/zhuxuehui/git/GitTest2.java
@@ -0,0 +1,10 @@
+package com.zhuxuehui.git;
+
+/**
+ * @author toto
+ * @version 1.0
+ * @date 2021-05-05 18:10
+ * @description
+ */
+public class GitTest2 {
+}
